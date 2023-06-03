87b0c64f2568f006e16fadcef036e214c0583f75
Asilbek Ruzikulov <asilbek.tech@gmail.com>
2023-06-03 18:37:22 -0400

    Finish assignment-3

--- assignments/assignment-3/script.js
+++ assignments/assignment-3/script.js
@@ -1,11 +1,60 @@
 console.log(5);
 var num1 = 5;
 var num2 = 10;
+
 var sum = num1 + num2;
 console.log(sum);
+
 var subtract = num1 - num2;
 console.log(subtract);
+
 var multiply = num1 * num2;
 console.log(multiply);
+
 var divide = num1 / num2;
 console.log(divide);
+
+//
+
+var fahrenheit = 100;
+var celsius = (fahrenheit - 32) * (5 / 9);
+console.log(celsius);
+
+var celsius = 80;
+var fahrenheit = celsius * (9 / 5) + 32;
+console.log(fahrenheit);
+
+//
+
+var principal = 50;
+var rate = 5;
+var time = 10;
+
+var interest = (principal * rate * time) / 100;
+console.log(interest);
+
+//
+
+var m3 = 500;
+var srt = 700;
+m3 = m3 + srt;
+
+srt = m3 - srt;
+m3 = m3 - srt;
+
+console.log(m3);
+
+console.log(srt);
+
+//
+
+var radius = 10;
+var area = (3.14 * 10) ^ 2;
+console.log(area);
+
+var length = 2;
+var width = 5;
+
+perimeter = 2 * (length + width);
+
+console.log(perimeter);
