a64fccf66355fb2d41e7c268e2fa478f00193eed
Asilbek Ruzikulov <asilbek.tech@gmail.com>
2023-06-05 07:13:57 -0400

    Finish assignment-5

---
+++ assignments/assignment-5/script.js
@@ -0,0 +1,14 @@
+var name = "Alex";
+var age = 25;
+var cityOfResidence = "New York";
+var profession = "Software engineer";
+var hobby = "Soccer";
+var favoriteFood = "Pizza";
+var favoriteColor = "Black";
+var movie = "Fast and Furious";
+var dreamDestination = "Uzbekistan";
+var favoriteSeason = "autumn";
+
+console.log(
+    `Hello, my name is ${name} and I am ${age} years old. I live in ${cityOfResidence}, where I work as a ${profession}. In my free time, I enjoy reading books and playing ${hobby}. My favorite food is pizza and my favorite color is ${favoriteColor}. My dream is to one day visit ${dreamDestination}. My favorite season of the year is ${favoriteSeason} because of the beautiful changing colors of the leaves`
+);
