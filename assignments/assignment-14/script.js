1e0f2a385a34810a1c8f6c662ca5c60d294b05a8
Asilbek Ruzikulov <asilbek.tech@gmail.com>
2023-06-13 16:18:19 -0400

    Finish assignment-14

--- assignments/assignment-14/script.js
+++ assignments/assignment-14/script.js
@@ -24,13 +24,14 @@ for (var i = 6; i <= 76; i++) {
 
 // part3
 
-function numbersDivisibleBy3and5() {
+function numbersDivisibleBythreeandfive() {
     for (var i = 1; i <= 99; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
             console.log(i);
         }
     }
 }
+
 for (var i = 1; i <= 99; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log(i);
@@ -38,3 +39,33 @@ for (var i = 1; i <= 99; i++) {
 }
 
 console.log(i);
+
+// part4
+
+function calculateAndPrint() {
+    for (var i = 20; i <= 40; i++) {
+        var printSquares = i * i;
+        console.log(printSquares);
+    }
+}
+
+for (var i = 20; i <= 40; i++) {
+    var printSquares = i * i;
+    console.log(printSquares);
+}
+
+// part5
+
+function printNumbersDivisible() {
+    for (var i = 1; i <= 99; i++) {
+        if ((i % 3 === 0 && i % 5 !== 0) || (i % 3 !== 0 && i % 5 === 0)) {
+            console.log(i);
+        }
+    }
+}
+
+for (var i = 1; i <= 99; i++) {
+    if ((i % 3 === 0 && i % 5 !== 0) || (i % 3 !== 0 && i % 5 === 0)) {
+        console.log(i);
+    }
+}
